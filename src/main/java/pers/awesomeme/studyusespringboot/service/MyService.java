cdb98a3b3c63ec18f97dd1eab27fbafbca0a7969
awesome <fakemail@outlook.com>
2025-02-12 21:31:33 +0800

    feat

--- src/main/java/pers/awesomeme/studyusespringboot/service/MyService.java
+++ src/main/java/pers/awesomeme/studyusespringboot/service/MyService.java
@@ -2,14 +2,16 @@ package pers.awesomeme.studyusespringboot.service;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Console;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import pers.awesomeme.commoncode.ApiResp;
 
 @Service
+@RefreshScope // 在这个bean中开启自动更新
 public class MyService
 {
-    @NacosValue(value = "${my.a}", autoRefreshed = true)
+    @Value("${my.a}") // 使用 @Value 注解
     private String theVal;
 
     public ApiResp<Long> getCurTimestamp()
